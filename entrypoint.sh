d294efd82d3351dc9fa5dd9866139cb1260d87f6
404invalid-user <invaliduser@bruvland.com>
2025-10-20 17:44:01 +0100

    nginx 8.4

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sleep 1
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+${MODIFIED_STARTUP}
+
+rm -rf /home/container/tmp/*; 
+
+echo "Starting PHP-FPM";
+/usr/sbin/php-fpm84 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize;
+
+echo "Starting Nginx";
+
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/;
+
+echo "Successfully started"; 
