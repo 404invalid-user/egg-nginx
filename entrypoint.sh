81115869491fc05629780a9077f43f50c796ec9f
404invalid-user <invaliduser@bruvland.com>
2025-10-20 09:34:15 +0100

    fix?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 sleep 1
 cd /home/container
 
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval "$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 ${MODIFIED_STARTUP}
