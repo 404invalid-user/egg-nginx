6dba2c152c84b61d825f6e7747336712467a0f39
404invalid-user <invaliduser@bruvland.com>
2025-10-20 17:55:19 +0100

    fix?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 sleep 1
 cd /home/container
 
-# Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+
 ${MODIFIED_STARTUP}
+
+sleep 1;
 
 rm -rf /home/container/tmp/*; 
 
