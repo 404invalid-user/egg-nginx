2f5405926a1124ed4ea76e32643b4612476507fc
404invalid-user <invaliduser@bruvland.com>
2025-10-20 10:05:16 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,19 @@
 sleep 1
 cd /home/container
 
-MODIFIED_STARTUP=`eval "$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"`
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 ${MODIFIED_STARTUP}
+
+rm -rf /home/container/tmp/*; 
+
+echo "Starting PHP-FPM";
+/usr/sbin/php-fpm82 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize;
+
+echo "Starting Nginx";
+
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/;
+
+echo "Successfully started"; 
